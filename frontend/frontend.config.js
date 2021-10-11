876873d4495596670749e4c981287d6fd7d833f0
Karthikeyan <karthikeyan@cere.io>
2021-10-11 12:22:05 +0530

    fix(local config): local config

--- frontend/frontend.config.js
+++ frontend/frontend.config.js
@@ -1,18 +1,18 @@
 export const network = {
-  id: 'kusama',
-  name: 'Kusama',
-  tokenSymbol: 'KSM',
-  tokenDecimals: 12,
-  ss58Format: 2,
-  coinGeckoDenom: 'kusama',
-  nodeWs: 'wss://kusama-rpc.polkadot.network',
-  backendWs: 'wss://kusama.polkastats.io/api/v3',
-  backendHttp: 'https://kusama.polkastats.io/api/v3',
-  googleAnalytics: 'UA-172854168-1',
+  id: 'cere',
+  name: 'Cerebellum Network',
+  tokenSymbol: 'CERE',
+  tokenDecimals: 10,
+  ss58Format: -1,
+  coinGeckoDenom: 'cere',
+  nodeWs: 'wss://archive.testnet.cere.network:9945',
+  backendWs: 'ws://localhost:8082/v1/graphql',
+  backendHttp: 'http://localhost:8080/api/v1',
+  googleAnalytics: '',
   theme: '@/assets/scss/themes/polkastats.scss',
-  // ranking
-  historySize: 84, // 21 days
-  erasPerDay: 4,
+  // ranking               Í
+  historySize: 84, // 84 days
+  erasPerDay: 1,
   validatorSetSize: 24,
 }
 export const paginationOptions = [10, 20, 50, 100]
