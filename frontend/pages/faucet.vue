1fb74c3fed00c17d39866e3f4dc53ba9940bd702
Karthikeyan <karthikeyan@cere.io>
2021-10-21 23:38:55 +0530

    feat(faucet): upated ui

--- frontend/pages/faucet.vue
+++ frontend/pages/faucet.vue
@@ -5,7 +5,7 @@
         <b-row>
           <b-col cols="12">
             <h1>
-              {{ $t('pages.faucets.faucet') }}
+              {{ $t('pages.faucets.title') }}
             </h1>
           </b-col>
         </b-row>
@@ -14,7 +14,7 @@
             <h6>Select Network:</h6>
           </b-col>
           <b-col cols="10">
-            <select v-model="network" class="custom-select">
+            <select v-model="networkValue" class="custom-select">
               <option value="testnet">Testnet</option>
               <option value="devnet">Devnet</option>
               <option value="qanet">QAnet</option>
@@ -32,24 +32,95 @@
           </b-col>
         </b-form-row>
         <b-form-row class="flex">
-          <b-button class="button" @click="api">Button</b-button>
+          <b-button
+            :disabled="disableButton"
+            class="button"
+            @click="requestAsset"
+          >
+            <span>Send me test CERE</span>
+          </b-button>
         </b-form-row>
+        <div>
+          <b-alert
+            :show="dismissCountDown"
+            dismissible
+            :variant="alertType"
+            class="mt-5"
+            @dismissed="dismissCountDown = 0"
+            @dismiss-count-down="countDownChanged"
+          >
+            {{ alertMessage }}
+          </b-alert>
+        </div>
       </b-container>
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { network } from '@/frontend.config.js'
 export default {
   data() {
     return {
-      network: 'testnet',
+      network,
+      networkValue: 'testnet',
       address: null,
+      disableButton: false,
+      dismissSecs: 5,
+      dismissCountDown: 0,
+      alertType: null,
+      alertMessage: null,
+    }
+  },
+  head() {
+    return {
+      title: this.$t('pages.faucets.head_title'),
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$t('pages.faucets.head_content'),
+        },
+      ],
     }
   },
   methods: {
-    api(event) {
-      alert('iamcalled')
+    requestAsset() {
+      this.disableButton = true
+      axios
+        .post('http://localhost:8000/api/v1/faucet', {
+          address: this.address,
+          network: this.networkValue,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            this.alertType = 'success'
+            this.alertMessage = `Congrats! Your transaction has been put in block successfully.
+            Please wait about 15-20 seconds in order to transaction being
+            finalized.`
+            this.showAlert()
+          }
+        })
+        .catch((error) => {
+          if (error.response.status === 400) {
+            this.alertType = 'danger'
+            this.alertMessage = error.response.data.msg
+            this.showAlert()
+          }
+        })
+    },
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+      console.log(this.dismissCountDown)
+      if (this.dismissCountDown === 0) {
+        this.address = null
+        this.networkValue = 'testnet'
+        this.disableButton = false
+      }
+    },
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs
     },
   },
 }
