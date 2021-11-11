a767503f0d97ff45987db18c49a6bdc8af022890
karthikeyan-cere <karthikeyan@cere.io>
2021-11-11 15:56:46 +0530

    feat(chart): add cumulative chart for signed extrinsic

--- frontend/components/charts/LineChart.js
+++ frontend/components/charts/LineChart.js
@@ -3,6 +3,18 @@ export default {
   extends: Line,
   props: ['data', 'options'],
   mounted() {
-    this.renderChart(this.data, this.options)
+    // this.renderChart(this.data, this.options)
+    this.renderLineChart()
+  },
+  methods: {
+    renderLineChart() {
+      this.renderChart(this.data, this.options)
+    },
+  },
+  watch: {
+    data() {
+      // this._chart.destroy()
+      this.renderLineChart()
+    },
   },
 }
---
+++ frontend/pages/chart.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <section>
+      <b-container class="main py-5">
+        <b-row class="mb-2">
+          <b-col cols="12">
+            <h1>{{ $t('pages.chart.title') }}</h1>
+          </b-col>
+        </b-row>
+        <b-col>
+          <ExtrinsicChart :extrinsic="dataToShow" />
+        </b-col>
+      </b-container>
+    </section>
+  </div>
+</template>
+
+<script>
+import { gql } from 'graphql-tag'
+import ExtrinsicChart from '@/components/validator/charts/ExtrinsicChart.vue'
+
+export default {
+  components: {
+    ExtrinsicChart,
+  },
+  data() {
+    return {
+      dataToShow: [],
+    }
+  },
+  head() {
+    return {
+      title: this.$t('pages.chart.head_title'),
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Cere Stats',
+        },
+      ],
+    }
+  },
+  apollo: {
+    $subscribe: {
+      banner: {
+        query: gql`
+          query count {
+            extrinsic_count(limit: 30) {
+              count
+              day
+            }
+          }
+        `,
+        result({ data }) {
+          data.extrinsic_count.forEach((element) => {
+            this.dataToShow.push({ count: element.count, day: element.day })
+          })
+        },
+      },
+    },
+  },
+}
+</script>
+
+<style>
+</style>
