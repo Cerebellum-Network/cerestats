aed3d071bb12bed299975fc5937aae75167128c7
Maksim Ramanenkau <cbnhrpo@gmail.com>
2024-06-06 10:34:52 +0200

    Make Historical Rewards table adaptive

--- frontend/pages/cluster/_id.vue
+++ frontend/pages/cluster/_id.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section>
-      <b-container class="main py-5">
+      <b-container class="page-cluster main py-5">
         <b-row class="mb-2">
           <b-col cols="12">
             <h1>
@@ -12,13 +12,19 @@
         <b-container class="main mb-5 p-0">
           <b-row>
             <b-col>
-              <div class="widget mb-4">
+              <div class="widget mw240 mb-4">
                 <div class="col-10">
                   <h5 class="widget-title mb-2">
                     {{ $t('pages.cluster.cluster_id') }}
                   </h5>
                   <h5>
-                    {{ clusterId }}
+                    <Identicon :address="clusterId" :size="20" />
+                    <nuxt-link
+                      :to="`/account/${clusterId}`"
+                      :title="$t('pages.cluster.node_provider_id')"
+                    >
+                      {{ shortAddress(clusterId) }}
+                    </nuxt-link>
                   </h5>
                 </div>
               </div>
@@ -36,7 +42,7 @@
               </div>
             </b-col>
             <b-col>
-              <div class="widget mb-4">
+              <div class="widget mw240 mb-4">
                 <div class="col-10">
                   <h5 class="widget-title mb-2">
                     {{ $t('pages.cluster.throughput') }}
@@ -53,12 +59,77 @@
         <template v-else>
           <div>
             <b-table
-              id="accounts-table"
+              id="cluster-table"
               striped
               stacked="md"
               :fields="fields"
               :items="clusterStats"
             >
+              <template #cell(node_provider_id)="data">
+                <div
+                  class="d-block d-sm-block d-md-none d-lg-none d-xl-none text-center"
+                >
+                  <Identicon :address="data.item.node_provider_id" :size="40" />
+                  <nuxt-link
+                    :to="`/account/${data.item.node_provider_id}`"
+                    :title="$t('pages.cluster.node_provider_id')"
+                  >
+                    {{ shortAddress(data.item.node_provider_id) }}
+                  </nuxt-link>
+                  <table class="table table-striped mt-4">
+                    <tbody>
+                      <tr>
+                        <td class="text-left">
+                          <strong>{{ $t('pages.cluster.rewards') }}</strong>
+                        </td>
+                        <td class="text-right">
+                          {{ data.item.rewarded }}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="text-left">
+                          <strong>{{ $t('pages.cluster.gets') }}</strong>
+                        </td>
+                        <td class="text-right">
+                          {{ data.item.number_of_gets }}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td class="text-left">
+                          <strong>{{ $t('pages.cluster.puts') }}</strong>
+                        </td>
+                        <td class="text-right">
+                          {{ data.item.number_of_puts }}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div class="d-none d-sm-none d-md-block d-lg-block d-xl-block">
+                  <div>
+                    <Identicon
+                      :address="data.item.node_provider_id"
+                      :size="20"
+                    />
+                    <nuxt-link
+                      v-b-tooltip.hover
+                      :to="`/account/${data.item.node_provider_id}`"
+                      :title="$t('pages.cluster.node_provider_id')"
+                    >
+                      {{ shortAddress(data.item.node_provider_id) }}
+                    </nuxt-link>
+                  </div>
+                </div>
+              </template>
+              <template #cell(rewarded)="data">
+                <p class="text-right mb-0">{{ data.item.rewarded }}</p>
+              </template>
+              <template #cell(number_of_gets)="data">
+                <p class="text-right mb-0">{{ data.item.number_of_gets }}</p>
+              </template>
+              <template #cell(number_of_puts)="data">
+                <p class="text-right mb-0">{{ data.item.number_of_puts }}</p>
+              </template>
             </b-table>
           </div>
         </template>
@@ -80,23 +151,111 @@
             </div>
             <template v-else>
               <div class="table-responsive">
-                <b-table striped hover :fields="rewardsFields" :items="rewards">
+                <b-table
+                  id="rewards-table"
+                  striped
+                  hover
+                  :fields="rewardsFields"
+                  :items="rewards"
+                >
+                  <template #cell(provider)="data">
+                    <div
+                      class="d-block d-sm-block d-md-none d-lg-none d-xl-none text-center"
+                    >
+                      <Identicon
+                        :address="data.item.node_provider_id"
+                        :size="40"
+                      />
+                      <nuxt-link
+                        :to="`/account/${data.item.node_provider_id}`"
+                        :title="$t('pages.cluster.provider')"
+                      >
+                        {{ shortAddress(data.item.node_provider_id) }}
+                      </nuxt-link>
+                      <table class="table table-striped mt-4">
+                        <tbody>
+                          <tr>
+                            <td class="text-left">
+                              <strong>{{ $t('pages.cluster.block') }}</strong>
+                            </td>
+                            <td class="text-right">
+                              <a
+                                :href="`https://explorer.cere.network/?rpc=wss%3A%2F%2Farchive.devnet.cere.network%2Fws#/explorer/query/${data.item.block_number}`"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                {{ data.item.block_number }}
+                              </a>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td class="text-left">
+                              <strong>{{ $t('pages.cluster.event') }}</strong>
+                            </td>
+                            <td class="text-right">
+                              {{ data.item.event_index }}
+                            </td>
+                          </tr>
+                          <tr>
+                            <td class="text-left">
+                              <strong>{{ $t('pages.cluster.era') }}</strong>
+                            </td>
+                            <td class="text-right">
+                              {{ data.item.era }}
+                            </td>
+                          </tr>
+                          <tr>
+                            <td class="text-left">
+                              <strong>{{
+                                $t('pages.cluster.reward_amount')
+                              }}</strong>
+                            </td>
+                            <td class="text-right">
+                              {{ formatAmount(data.item.rewarded) }}
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div
+                      class="d-none d-sm-none d-md-block d-lg-block d-xl-block"
+                    >
+                      <Identicon
+                        :address="data.item.node_provider_id"
+                        :size="20"
+                      />
+                      <nuxt-link
+                        :to="`/account/${data.item.node_provider_id}`"
+                        :title="$t('pages.cluster.node_provider_id')"
+                      >
+                        {{ shortAddress(data.item.node_provider_id) }}
+                      </nuxt-link>
+                    </div>
+                  </template>
                   <template #cell(block)="data">
-                    <p class="mb-0">{{ data.item.block_number }}</p>
+                    <div class="text-right mb-0">
+                      <a
+                        :href="`https://explorer.cere.network/?rpc=wss%3A%2F%2Farchive.devnet.cere.network%2Fws#/explorer/query/${data.item.block_number}`"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {{ data.item.block_number }}
+                      </a>
+                    </div>
                   </template>
                   <template #cell(event)="data">
-                    <p class="mb-0">
+                    <p class="text-right mb-0">
                       {{ data.item.event_index }}
                     </p>
                   </template>
-                  <template #cell(provider)="data">
-                    <p class="mb-0">
-                      {{ data.item.node_provider_id }}
+                  <template #cell(era)="data">
+                    <p class="text-right mb-0">
+                      {{ data.item.era }}
                     </p>
                   </template>
                   <template #cell(reward)="data">
-                    <p class="mb-0">
-                      {{ data.item.rewarded }}
+                    <p class="text-right mb-0">
+                      {{ formatAmount(data.item.rewarded) }}
                     </p>
                   </template>
                 </b-table>
@@ -185,53 +344,57 @@ export default {
           key: 'node_provider_id',
           label: this.$t('pages.cluster.node_provider_id'),
           sortable: true,
-        },
-        {
-          key: 'node_type',
-          label: this.$t('pages.cluster.node_type'),
-          sortable: true,
+          thStyle: { width: '23%' },
         },
         {
           key: 'rewarded',
           label: this.$t('pages.cluster.rewards'),
           sortable: true,
+          class: `d-none d-sm-none d-md-table-cell d-lg-table-cell d-xl-table-cell`,
         },
         {
           key: 'number_of_gets',
           label: this.$t('pages.cluster.gets'),
           sortable: true,
+          class: `d-none d-sm-none d-md-table-cell d-lg-table-cell d-xl-table-cell`,
         },
         {
           key: 'number_of_puts',
           label: this.$t('pages.cluster.puts'),
           sortable: true,
+          class: `d-none d-sm-none d-md-table-cell d-lg-table-cell d-xl-table-cell`,
         },
       ],
       rewardsFields: [
         {
+          key: 'provider',
+          label: this.$t('pages.cluster.rewarded_provider'),
+          sortable: true,
+          thStyle: { width: '25%' },
+        },
+        {
           key: 'block',
           label: this.$t('pages.cluster.block'),
           sortable: true,
+          class: `d-none d-sm-none d-md-table-cell d-lg-table-cell d-xl-table-cell`,
         },
         {
           key: 'event',
           label: this.$t('pages.cluster.event_index'),
           sortable: true,
-        },
-        {
-          key: 'provider',
-          label: this.$t('pages.cluster.rewarded_provider'),
-          sortable: true,
+          class: `d-none d-sm-none d-md-table-cell d-lg-table-cell d-xl-table-cell`,
         },
         {
           key: 'era',
           label: this.$t('pages.cluster.era'),
           sortable: true,
+          class: `d-none d-sm-none d-md-table-cell d-lg-table-cell d-xl-table-cell`,
         },
         {
           key: 'reward',
           label: this.$t('pages.cluster.reward_amount'),
           sortable: true,
+          class: `d-none d-sm-none d-md-table-cell d-lg-table-cell d-xl-table-cell`,
         },
       ],
       providers: [],
@@ -289,7 +452,6 @@ export default {
           query node_provider_stats($clusterId: String) {
             node_provider_stats(where: { cluster_id: { _eq: $clusterId } }) {
               node_provider_id
-              node_type
             }
           }
         `,
